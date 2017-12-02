c9b7182853ebb161c93205f0c846eaaf2db2ea72
Aleksandr Plesovskikh <me@hgen.ru>
2017-12-02 21:15:48 +0300

    Add basic EvolutionChamber implementation

--- evolution_chamber/__tests__/test_evolution_chamber.py
+++ evolution_chamber/__tests__/test_evolution_chamber.py
@@ -1,10 +1,119 @@
+import collections
+import numpy as np
 from nose.tools import ok_, eq_
+from evolution_chamber.evolution_chamber import EvolutionChamber, EvolveResult
 
-from evolution_chamber.evolution_chamber import EvolutionChamber
+
+class Model:
+    def __init__(self):
+        self.X_ = None
+        self.y_ = None
+        self.score_ = None
+
+    def fit(self, X, Y):
+        self.X_ = X
+        self.y_ = y
+
+    def score(self, x_test, y_test):
+        self.score_ = 0
+        return self.score_
+
+    def make_child(self):
+        return type(self)()
 
 
-def test_init():
-    class Test:
-        pass
-    evo_chamber = EvolutionChamber(Test)
+class Winner(Model):
+    def score(self, x_test, y_test):
+        self.score_ = 100
+        return self.score_
+
+
+class SecondGenWinner(Model):
+    def make_child(self):
+        return Winner()
+
+
+default_initial_individuals = (Model() for x in range(0, 10))
+X = np.array([[i] for i in range(0, 100)])
+y = np.array([[i] for i in range(0, 100)])
+
+
+def test_evolution_chamber_init():
+    evo_chamber = EvolutionChamber(X=X, y=y)
     ok_(isinstance(evo_chamber, EvolutionChamber))
+
+
+def test_evolve_should_return_iterable():
+    evo_chamber = EvolutionChamber(X=X, y=y)
+    evolve_iterator = evo_chamber.evolve(
+        default_initial_individuals,
+        iteratons=10,
+        limit_individuals=10
+    )
+    ok_(isinstance(evolve_iterator, collections.Iterable))
+
+
+def test_evolve_should_return_evolve_result():
+    evo_chamber = EvolutionChamber(X=X, y=y)
+    evolve_iterator = evo_chamber.evolve(
+        default_initial_individuals,
+        iteratons=10,
+        limit_individuals=10
+    )
+    evolve_result = next(evolve_iterator)
+    ok_(isinstance(evolve_result, EvolveResult))
+
+
+def test_evolve_model_fit():
+    evo_chamber = EvolutionChamber(X=X, y=y)
+    evolution_iter = evo_chamber.evolve(
+        [Model()],
+        iteratons=10,
+        limit_individuals=10
+    )
+    evolution_result = next(evolution_iter)
+    individual = evolution_result.population[0]
+    X_values = np.array(individual.X_).flatten()
+    are_all_values_from_first_batch = (X_values < 10).all()
+    ok_(are_all_values_from_first_batch, 'model should be fitted with the first batch')
+
+
+def test_evolve_winner_on_first_iteration():
+    evo_chamber = EvolutionChamber(X=X, y=y)
+    evolution_iter = evo_chamber.evolve(
+        [Winner(), Model()],
+        iteratons=10,
+        limit_individuals=10
+    )
+    evolution_result = next(evolution_iter)
+    winner = evolution_result.population[0]
+    eq_(winner.score_, 100)
+
+
+def test_evolve_winner_on_second_iteration():
+    evo_chamber = EvolutionChamber(X=X, y=y)
+    evolution_iter = evo_chamber.evolve(
+        [Model(), SecondGenWinner()],
+        iteratons=10,
+        limit_individuals=10
+    )
+    first_evolution_result = next(evolution_iter)
+    first_winner = first_evolution_result.population[0]
+    eq_(first_winner.score_, 0)
+    second_evolution_result = next(evolution_iter)
+    second_winner = second_evolution_result.population[0]
+    eq_(second_winner.score_, 100)
+
+
+def test_evolve_survivors():
+    evo_chamber = EvolutionChamber(X=X, y=y)
+    evolution_iter = evo_chamber.evolve(
+        [Winner(), Winner(), Model()],
+        iteratons=10,
+        limit_individuals=2
+    )
+    next(evolution_iter)
+    second_result = next(evolution_iter)
+    eq_(len(second_result.survivors), 2)
+    eq_(second_result.survivors[0].score_, 100)
+    eq_(second_result.survivors[1].score_, 100)
--- evolution_chamber/evolution_chamber.py
+++ evolution_chamber/evolution_chamber.py
@@ -1,3 +1,41 @@
-class EvolutionChamber():
-    def __init__(self, Model):
+import collections
+from sklearn.model_selection import train_test_split
+
+
+class EvolveResult():
+    def __init__(self, population, survivors):
+        self.population = population
+        self.survivors = survivors
+
+
+class EvolutionChamber:
+    def __init__(self, X=None, y=None):
+        self._X = X
+        self._y = y
+
+    def evolve(
+        self,
+        initial_individuals: collections.Iterable,
+        iteratons: int = 100,
+        test_size: int = 0.3,
+        limit_individuals: int = 100
+    ) -> collections.Iterator:
+        individuals = initial_individuals
+        for iteration in range(1, iteratons + 1):
+            X_train, X_test, y_train, y_test = self._split_data(test_size, iteration, iteratons)
+            for individual in individuals:
+                individual.fit(X_train, y_train)
+                individual.score(X_test, y_test)
+            population = sorted(individuals, key=lambda i: i.score_, reverse=True)
+            survivors = population[0:limit_individuals]
+            individuals = [individual.make_child() for individual in survivors]
+            yield EvolveResult(population, survivors)
+
+    def _split_data(self, test_size, current_iteration, all_iterations):
+        to = (len(self._X) // all_iterations) * current_iteration
+        current_X = self._X[0:to]
+        current_y = self._y[0:to]
+        return train_test_split(current_X, current_y, test_size=test_size)
+
+    def _next_gen(self, individuals, limit):
         pass
