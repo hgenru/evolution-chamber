49b0d877cc57df0f890c462e19b756238651b945
Artem <aionics@list.ru>
2017-12-04 18:13:58 +0500

    simple mutations added

--- evolution_chamber/models.py
+++ evolution_chamber/models.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 import numpy as np
+import math
+import random
 
 class SimpleLayer(nn.Module):
 	def __init__(self, layer_size, activation=False):
@@ -11,36 +13,42 @@ class SimpleLayer(nn.Module):
 		self.activation = activation
 	def forward(self, x):
 		return self.linear( x ) if not self.activation else self.activation( self.linear( x ) )
-
+		
 
 class NetModel(nn.Module):
-    def __init__(self, layer_sizes):
-        super(NetModel, self).__init__()
-        self.layer_list = []
-        for layer_size in layer_sizes:
-            self.layer_list.append(SimpleLayer(layer_size))
+	def __init__(self, layer_sizes):
+		super(NetModel, self).__init__()
+		self.layer_list = []
+		for layer_size in layer_sizes:
+			self.layer_list.append(SimpleLayer(layer_size))
 
-        self.net = nn.Sequential(*self.layer_list)
+		self.net = nn.Sequential(*self.layer_list)
 
-    def forward(self, x):
-        return self.net( x )
+	def forward(self, x):
+		return self.net( x )
 
-    def get_score(self):
-        pass
+	def get_score(self):
+		pass
 
-    def make_child(self, mutate=True):
-        params = []
-        for name in self.state_dict():
-            if name.split('.')[-1] != 'bias':
-                params.append(self.state_dict()[name].size())
+	def make_child(self, mutate=False):
+		params = []
+		for name in self.state_dict():
+			if name.split('.')[-1] != 'bias':
+				params.append(list(self.state_dict()[name].size()))
 
-        return NetModel(params)
+		if mutate:
+			for layer in params:
+				for i, n in enumerate(layer):
+					layer[i] = int(n + math.floor( random.randint(0, n//5) - n//10 ))
+
+		return NetModel(layer_sizes=params)
+
 
 # model = NetModel([
-#     [10, 1]
+# 	[100, 1],
+# 	[100, 1],
+# 	[100, 1],
+# 	[100, 1],
 # ])
-# model(x)
-# model.forward(x)
 # child = model.make_child()
-# print(child.state_dict())
-# print([param in model.state_dict()])
+# mutated_child = model.make_child(mutate=True)
